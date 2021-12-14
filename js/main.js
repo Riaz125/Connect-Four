d0cd58f50a43f42cdaedb922aed42317ec1586fb
Riaz <riazzzz@gmail.com>
2021-12-13 16:58:16 -0800

    handleTurn function seems viable

--- js/main.js
+++ js/main.js
@@ -71,27 +71,46 @@ const winningCombos = [
     [13, 20, 27, 34],
   ]
 
+// let board = [
+//     [null,null,null,null,null,null],
+//     [null,null,null,null,null,null],
+//     [null,null,null,null,null,null],
+//     [null,null,null,null,null,null],
+//     [null,null,null,null,null,null],
+//     [null,null,null,null,null,null],
+//     [null,null,null,null,null,null],
+// ]
+
 let board = [
-    [null,null,null,null,null,null],
-    [null,null,null,null,null,null],
-    [null,null,null,null,null,null],
-    [null,null,null,null,null,null],
-    [null,null,null,null,null,null],
-    [null,null,null,null,null,null],
-    [null,null,null,null,null,null],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
 ]
 
 let playerOneGrid;
 let playerTwoGrid;
 let playerTurn;
 let winner;
-let columns = [ [0, 1, 2, 3, 4, 5],
-            [6, 7, 8, 9, 10, 11],
-            [12, 13, 14, 15, 16, 17],
-            [18, 19, 20, 21, 22, 23],
-            [24, 25, 26, 27, 28, 29],
-            [30, 31, 32, 33, 34, 35],
-            [36, 37, 38, 39, 40, 41] ]
+// let columns = [ [0, 1, 2, 3, 4, 5],
+//             [6, 7, 8, 9, 10, 11],
+//             [12, 13, 14, 15, 16, 17],
+//             [18, 19, 20, 21, 22, 23],
+//             [24, 25, 26, 27, 28, 29],
+//             [30, 31, 32, 33, 34, 35],
+//             [36, 37, 38, 39, 40, 41] ]
+let columns = [
+    [0,7,14,21,28,35],
+    [1,8,15,22,29,36],
+    [2,9,16,23,30,37],
+    [3,10,17,24,31,38],
+    [4,11,18,25,32,39],
+    [5,12,19,26,33,40],
+    [6,13,20,27,34,41]
+]
 
 document.querySelector("#board").addEventListener("click", handleTurn);
 
@@ -121,6 +140,18 @@ function handleTurn(e) {
                 return;
             } else {
             playerOneGrid.push(columns[column].shift());
+            board[column].push(1);
+            }
+            winningCombos.forEach(function(array) {
+                if(array.every(i => playerOneGrid.includes(i))) {
+                    console.log('you won!')
+                    return winner = 1;
+                }
+            });
+            if (winningCombos.forEach(function(array) {array.length === 0})) {
+                return winner = 3;
+            } else {
+                return playerTurn = 2;
             }
         } else if (playerTurn === 2) {
             let column = e.target.className;
@@ -128,8 +159,22 @@ function handleTurn(e) {
                 return;
             } else {
             playerTwoGrid.push(columns[column].shift());
+            board[column].push(2);
         }
-    }
+            winningCombos.forEach(function(array) {
+                if(array.every(i => playerOneGrid.includes(i))) {
+                    console.log('you won!')
+                    return winner = 2;
+                }
+            });
+            if (winningCombos.forEach(function(array) {array.length === 0})) {
+                return winner = 3;
+            } else {
+                return playerTurn = 1;
+            }
+        }
+
+        
 }
 // function render() {
 //     board.forEach(function(array) {
